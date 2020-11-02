2997ded5b2a800bb02ba2ada9846ebd8658a7375
John Lao <59539676+MrLao9038@users.noreply.github.com>
2020-11-02 15:41:24 -0500

    Update CreateMyPass.py

--- CreateMyPass.py
+++ CreateMyPass.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3        #Select interpreter for BASH
 
-import random
+import random                 #Module for generating pseudo-random number
 
 print("Generate Passwords For Me")
 print("                                                  ")
 
-print("--------------------------------------------------")
+print("--------------------------------------------------")     #Ascii Art
 print("eeeee eeeee eeeee eeeee e  e  e 8eee8 eeeee  eeeee")
 print("8   8 8   8 88    88    8  8  8 8   8 8   8  8   8")
 print("8eee8 8eee8 8eeee 8eeee 8  8  8 8   8 8eee8e 8e  8")
@@ -18,17 +18,18 @@ print("                                                  ")
 print("Here Are The Passwords")
 print("                                                  ")
 
-uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"            #List of letters, numbers, and special characters for password
 lowercase_letters = uppercase_letters.lower()
 numbers = "0123456789"
 special_characters = "@%+|?!#$^?:,(){}[]~_-./\\"
 
-upper, lower, nums, chars = True, True, True, True
+upper, lower, nums, chars = True, True, True, True          #Boolean statements to choose which items on list to be included
+                                                            #in password generating process
+                    
+all = ""      #Empty string to store our random generated password
 
-all = ""
-
-if upper:
-    all += uppercase_letters
+if upper:                                                   #Based on boolean statement, we can select what will be included
+    all += uppercase_letters                                #password
 if lower:
     all += lowercase_letters
 if nums:
@@ -36,9 +37,9 @@ if nums:
 if chars:
     all += special_characters
 
-length = 20
-amount = 10
+length = 20             #We can dictate the length of password, the longer the better
+amount = 10             #We can dictate how many password to generate at a given time 
 
 for ele in range(amount):
-    password = "".join(random.sample(all, length))
-    print(password)
+    password = "".join(random.sample(all, length))          #join all the random list of iterable into one string, based on the
+    print(password)                                         #length we specify
